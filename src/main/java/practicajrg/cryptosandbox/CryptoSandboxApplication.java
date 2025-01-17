bd3a6d2b3e7d57f509e430d2d5b77eda94c40495
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2025-01-17 09:47:17 +0100

    Start Project Point

---
+++ src/main/java/practicajrg/cryptosandbox/CryptoSandboxApplication.java
@@ -0,0 +1,13 @@
+package practicajrg.cryptosandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/practicajrg/cryptosandbox/CryptoSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package practicajrg.cryptosandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
