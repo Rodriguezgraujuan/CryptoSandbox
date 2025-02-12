622f788198c1b409bba535bb9580fe0bbac5b57a
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2025-02-12 13:30:57 +0100

    BeanStalk

--- src/main/java/practicajrg/cryptosandbox/CryptoSandboxApplication.java
+++ src/main/java/practicajrg/cryptosandbox/CryptoSandboxApplication.java
@@ -2,14 +2,22 @@ package practicajrg.cryptosandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class CryptoSandboxApplication {
+public class CryptoSandboxApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CryptoSandboxApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CryptoSandboxApplication.class);
     }
 
 }
