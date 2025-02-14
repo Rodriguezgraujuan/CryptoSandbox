2a59988da368063752ee2ffd2d565f79186c8ec4
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2025-02-14 09:08:10 +0100

    Login solved

--- src/main/java/practicajrg/cryptosandbox/Service/CustomUserDetailsService.java
+++ src/main/java/practicajrg/cryptosandbox/Service/CustomUserDetailsService.java
@@ -14,18 +14,28 @@ import practicajrg.cryptosandbox.entities.Usuario;
 public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository usuarioRepository;
-
+    static int contador=0;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        try {
         Usuario usuario = usuarioRepository.findByEmail(email);
         if (usuario == null) {
             throw new BadCredentialsException("Credenciales incorrectas");
+        }else{
+            contador++;
         }
-        return User.withUsername(usuario.getEmail())
-                .password(usuario.getPassword())
-                .roles(usuario.getRol())
-                .build();
+        if (contador>=2){
+            contador=0;
+            throw new BadCredentialsException("Credenciales incorrectas");
+        }
+            return User.withUsername(usuario.getEmail())
+                    .password(usuario.getPassword())
+                    .roles(usuario.getRol())
+                    .build();
+        } catch (Exception e) {
+            throw new BadCredentialsException("Credenciales incorrectas");
+        }
     }
 
 }
