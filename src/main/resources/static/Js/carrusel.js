3d28b92918f12754c621faf9706080f512f6064b
marclecha05 <lechamarc05@gmail.com>
2025-02-19 10:52:31 +0100

    arreglar lighthouse

--- src/main/resources/static/Js/carrusel.js
+++ src/main/resources/static/Js/carrusel.js
@@ -9,7 +9,7 @@ function renderCarrusel(cryptos){
     let carrusel = $("#carrusel");
 
     // Crear los elementos con el mismo espaciado
-    let cryptoItems = cryptos.map(crypto => `<span>${crypto.name} <span class="text-warning">${crypto.value} EUR</span></span>`).join('');
+    let cryptoItems = cryptos.map(crypto => `<span>${crypto.name} <span class="bg-warning text-black p-3 mb-2">${crypto.value} EUR</span></span>`).join('');
 
     // Duplicar los elementos para que el carrusel sea infinito
     carrusel.html(cryptoItems + cryptoItems); // Duplica para hacer el efect
