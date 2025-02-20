4ac80a9555770b2554c6630d3dc5362ea0b7a571
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2025-02-20 09:22:19 +0100

    Cambio de puerto a 443

--- src/main/java/practicajrg/cryptosandbox/TomcatConfig.java
+++ src/main/java/practicajrg/cryptosandbox/TomcatConfig.java
@@ -17,9 +17,9 @@ public class TomcatConfig {
     private Connector createHttpConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        connector.setPort(8080); // Puerto HTTP
+        connector.setPort(8080);
         connector.setSecure(false);
-        connector.setRedirectPort(8443); // Redirigir a HTTPS
+        connector.setRedirectPort(443);
         return connector;
     }
 }
