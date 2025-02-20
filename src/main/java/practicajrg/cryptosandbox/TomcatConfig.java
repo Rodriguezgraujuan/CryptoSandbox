43d57a89be1a83aa3b3624d7be4f291a35c4d39a
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2025-02-20 09:15:17 +0100

    SSL

---
+++ src/main/java/practicajrg/cryptosandbox/TomcatConfig.java
@@ -0,0 +1,25 @@
+package practicajrg.cryptosandbox;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+        return factory -> factory.addAdditionalTomcatConnectors(createHttpConnector());
+    }
+
+    private Connector createHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080); // Puerto HTTP
+        connector.setSecure(false);
+        connector.setRedirectPort(8443); // Redirigir a HTTPS
+        return connector;
+    }
+}
