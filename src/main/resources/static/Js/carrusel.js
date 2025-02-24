7e3278229a082184edcbc2cba4691c31384b0463
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2025-02-24 10:08:03 +0100

    verificacion rojo

--- src/main/resources/static/Js/carrusel.js
+++ src/main/resources/static/Js/carrusel.js
@@ -7,10 +7,8 @@ $(document).ready(function () {
 })
 function renderCarrusel(cryptos){
     let carrusel = $("#carrusel");
-
     // Crear los elementos con el mismo espaciado
     let cryptoItems = cryptos.map(crypto => `<span>${crypto.name} <span class="bg-warning text-black p-3 mb-2 rounded-circle">${crypto.value} EUR</span></span>`).join('');
-
     // Duplicar los elementos para que el carrusel sea infinito
     carrusel.html(cryptoItems + cryptoItems); // Duplica para hacer el efect
 }
