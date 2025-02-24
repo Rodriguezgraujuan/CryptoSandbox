6dd4258fc5dfbf19b5980a7867cfbb201dfd5550
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2025-02-24 10:22:06 +0100

    verificacion rojo

--- src/main/resources/static/Js/eliminarUser.js
+++ src/main/resources/static/Js/eliminarUser.js
@@ -1,12 +1,33 @@
 $(document).ready( ()=>{
     $("#delete").on("click", (event) =>{
-        $.get("/delete", (data) => {
-        }).done((data) => {
-            window.location.href= "/logout"
-        }).fail((error)=>{
-            console.log("Usuario eliminado correctamente")
-            window.location.href= "/logout"
-        })
+        event.preventDefault()
+        $("#btn-eliminar").on("click", function(event) {
+            event.preventDefault(); // Evita que el formulario se envíe automáticamente si es un botón dentro de un formulario
+
+            Swal.fire({
+                title: "¿Estás seguro?",
+                text: "Esta acción no se puede deshacer.",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#d33",
+                cancelButtonColor: "#3085d6",
+                confirmButtonText: "Sí, eliminar",
+                cancelButtonText: "Cancelar"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $.get("/delete", (data) => {
+                    }).done((data) => {
+                        Swal.fire("¡Eliminado!", "El elemento ha sido eliminado.", "success");
+                        window.location.href= "/logout"
+                    }).fail((error)=>{
+                        console.log("Usuario eliminado correctamente")
+                        window.location.href= "/logout"
+                    })
+                }
+            });
+        });
+
+
     })
 })
 
