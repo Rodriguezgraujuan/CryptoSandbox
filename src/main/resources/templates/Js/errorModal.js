a430fed00c8348e31e2dbfd884a39a26fa0a4d86
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2025-02-24 11:44:48 +0100

    login extended js revert

--- src/main/resources/templates/Js/errorModal.js
+++
@@ -1,5 +0,0 @@
-let credentialsErrorModal = new bootstrap.Modal(document.getElementById('credentialsErrorModal'));
-let param = new URLSearchParams(window.location.search);
-if (param.get("error") === 'true') {
-    credentialsErrorModal.show();
-}
