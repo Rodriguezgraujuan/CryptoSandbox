765a59211fda4abc26eca40b3421bd8e485969c5
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2025-02-25 12:54:10 +0100

    redirecion

---
+++ src/main/java/practicajrg/cryptosandbox/controller/redirectController.java
@@ -0,0 +1,12 @@
+package practicajrg.cryptosandbox.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class redirectController {
+    @GetMapping("/")
+    public String redirectToHome() {
+        return "redirect:/invitado.html";
+    }
+}
